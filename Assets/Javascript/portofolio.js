aad5c08f857aff12353ee3cce866bea683b104c3
Fife <39037648+Chris6170@users.noreply.github.com>
2018-11-04 16:57:07 -0800

    updated

---
+++ Assets/Javascript/portofolio.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+
+
+    $('button').on('click', function () {
+        var value = $(this).val();
+        $(".code").hide();
+
+        if (value === "https://chris6170.github.io/Word-Guess-Game/") {
+            window.location = "https://chris6170.github.io/Word-Guess-Game/"
+
+        }
+
+        if (value === "https://github.com/Chris6170/Word-Guess-Game") {
+            window.location = "https://github.com/Chris6170/Word-Guess-Game"
+
+        }
+
+        if (value === "https://chris6170.github.io/unit-4-game/") {
+            window.location = "https://chris6170.github.io/unit-4-game/"
+
+        }
+
+        if (value === "https://github.com/Chris6170/unit-4-game") {
+            window.location = "https://github.com/Chris6170/unit-4-game"
+
+        }
+
+        if (value === "https://github.com/Chris6170/unit-4-harder-game") {
+            window.location = "https://github.com/Chris6170/unit-4-harder-game"
+
+        }
+
+        if (value === "https://chris6170.github.io/unit-4-harder-game/") {
+            window.location = "https://chris6170.github.io/unit-4-harder-game/"
+
+        }
+
+        if (value === "https://chris6170.github.io/Trivia_Game/") {
+            window.location = "https://chris6170.github.io/Trivia_Game/"
+
+        }
+
+        if (value === "https://github.com/Chris6170/Trivia_Game") {
+            window.location = "https://github.com/Chris6170/Trivia_Game"
+
+        }
+
+
+
+        if (value === "https://github.com/Chris6170/GifTastic") {
+            window.location = "https://github.com/Chris6170/GifTastic"
+
+        }
+
+        if (value === "https://chris6170.github.io/GifTastic/") {
+            window.location = "https://chris6170.github.io/GifTastic/"
+
+        }
+
+        if (value === "https://github.com/Chris6170/Project-1-API-") {
+            window.location = "https://github.com/Chris6170/Project-1-API-"
+
+        }
+
+        if (value === "https://chris6170.github.io/Project-1-API-/") {
+            window.location = "https://chris6170.github.io/Project-1-API-/"
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+    var queryurl = "http://quotes.rest/qod.json";
+
+    $.ajax({
+        url: queryurl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.contents.quotes[0].quote);
+        $("#test").html(response.contents.quotes[0].quote);
+    });
+
+
+
+
+
+
+});
+
+
