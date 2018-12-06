e4ea1043fd27c6bfc440a92585f717b231909446
Fife <39037648+Chris6170@users.noreply.github.com>
2018-12-06 07:56:27 -0800

    Javascript_fix

--- Assets/Javascript/portofolio.js
+++ Assets/Javascript/portofolio.js
@@ -5,84 +5,13 @@ $(document).ready(function () {
         var value = $(this).val();
         $(".code").hide();
 
-        if (value === "https://chris6170.github.io/Word-Guess-Game/") {
-            window.location = "https://chris6170.github.io/Word-Guess-Game/"
+        var projects = ["https://chris6170.github.io/Word-Guess-Game/", "https://github.com/Chris6170/Word-Guess-Game", "https://chris6170.github.io/unit-4-game/", "https://github.com/Chris6170/unit-4-game", "https://github.com/Chris6170/unit-4-harder-game", "https://chris6170.github.io/unit-4-harder-game/", "https://chris6170.github.io/Trivia_Game/", "https://github.com/Chris6170/Trivia_Game", "https://github.com/Chris6170/GifTastic", "https://chris6170.github.io/GifTastic/", "https://github.com/Chris6170/Project-1-API-", "https://chris6170.github.io/Project-1-API-/","https://github.com/Chris6170/liri-node-app","https://github.com/Chris6170/bamazon","https://github.com/Chris6170/Friend_finder"];
 
+        for (i = 0; i < projects.length; i++) {
+            if (projects[i] === value) {
+                window.location = projects[i];
+            }
         }
-
-        if (value === "https://github.com/Chris6170/Word-Guess-Game") {
-            window.location = "https://github.com/Chris6170/Word-Guess-Game"
-
-        }
-
-        if (value === "https://chris6170.github.io/unit-4-game/") {
-            window.location = "https://chris6170.github.io/unit-4-game/"
-
-        }
-
-        if (value === "https://github.com/Chris6170/unit-4-game") {
-            window.location = "https://github.com/Chris6170/unit-4-game"
-
-        }
-
-        if (value === "https://github.com/Chris6170/unit-4-harder-game") {
-            window.location = "https://github.com/Chris6170/unit-4-harder-game"
-
-        }
-
-        if (value === "https://chris6170.github.io/unit-4-harder-game/") {
-            window.location = "https://chris6170.github.io/unit-4-harder-game/"
-
-        }
-
-        if (value === "https://chris6170.github.io/Trivia_Game/") {
-            window.location = "https://chris6170.github.io/Trivia_Game/"
-
-        }
-
-        if (value === "https://github.com/Chris6170/Trivia_Game") {
-            window.location = "https://github.com/Chris6170/Trivia_Game"
-
-        }
-
-
-
-        if (value === "https://github.com/Chris6170/GifTastic") {
-            window.location = "https://github.com/Chris6170/GifTastic"
-
-        }
-
-        if (value === "https://chris6170.github.io/GifTastic/") {
-            window.location = "https://chris6170.github.io/GifTastic/"
-
-        }
-
-        if (value === "https://github.com/Chris6170/Project-1-API-") {
-            window.location = "https://github.com/Chris6170/Project-1-API-"
-
-        }
-
-        if (value === "https://chris6170.github.io/Project-1-API-/") {
-            window.location = "https://chris6170.github.io/Project-1-API-/"
-
-        }
-
-        if (value === "https://github.com/Chris6170/liri-node-app") {
-            window.location = "https://github.com/Chris6170/liri-node-app"
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
     })
 
     var queryurl = "https://quotes.rest/qod.json";
